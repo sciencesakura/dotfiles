be42b2a71d4702b9f7b79b58b8cc01df50ad37ef
sciencesakura <sakurasakurascience@gmail.com>
2020-10-15 14:39:30 +0900

    Add .npmrc

--- setup/setup.sh
+++ setup/setup.sh
@@ -6,7 +6,7 @@ curdir="$(dirname "$0")"
 curl -s https://get.sdkman.io | /bin/bash
 
 # create symlink for dotfiles
-for e in .vim bin .bash_profile .bashrc .gitconfig .gitignore_global .inputrc .tmux.conf .vimrc; do
+for e in .vim bin .bash_profile .bashrc .gitconfig .gitignore_global .inputrc .npmrc .tmux.conf .vimrc; do
   [ -e "$HOME/$e" ] && rm -rf "$HOME/$e"
   ln -s "$(realpath "$e")" "$HOME/"
 done
