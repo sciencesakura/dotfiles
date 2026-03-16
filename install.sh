7e5401b2458054419af83ca95c1e0d922bb8d70a
sciencesakura <sakurasakurascience@gmail.com>
2026-03-16 16:37:49 +0900

    Place local config files in the repo.

--- install.sh
+++ install.sh
@@ -1,21 +1,26 @@
 #!/bin/sh
 
-type realpath >/dev/null 2>&1 && \
-  REPO_ROOT="$(realpath "$(dirname "$0")")" || \
-  REPO_ROOT="$(cd "$(dirname "$0")" && pwd)"
+type realpath >/dev/null 2>&1 \
+    && REPO_ROOT="$(realpath "$(dirname "$0")")" \
+    || REPO_ROOT="$(cd "$(dirname "$0")" && pwd)"
 BACKUP_DIR="$HOME/backup/dotfiles/$(date +%Y%m%d%H%M%S)"
-ENTRIES=".bash_profile \
-  .bashrc \
-  .config/alacritty \
-  .config/git/config \
-  .config/tig \
-  .config/tmux/tmux.conf \
-  .config/tmux-powerline \
-  .config/vim \
-  .inputrc \
-  .profile"
+ENTRIES=".bash_aliases \
+    .bash_profile \
+    .bash_profile.local \
+    .bashrc \
+    .bashrc.local \
+    .config/alacritty \
+    .config/git \
+    .config/tig \
+    .config/tmux \
+    .config/tmux-powerline \
+    .config/vim \
+    .inputrc \
+    .profile \
+    .profile.local"
 
 for i in $ENTRIES; do
+  [ -e "$REPO_ROOT/$i" ] || continue
   printf "Installing %s\n" "$i"
   if [ -e "$HOME/$i" ]; then
     destdir="$BACKUP_DIR/$(dirname "$i")"
