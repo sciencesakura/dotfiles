8f0096742ea59f6fac90a4165ea32bc1fa0703f2
sciencesakura <sakurasakurascience@gmail.com>
2026-04-20 23:07:22 +0900

    cleanup gcloud config segment

--- .config/tmux-powerline/segments/gcloud_active_config.sh
+++ .config/tmux-powerline/segments/gcloud_active_config.sh
@@ -1,9 +1,16 @@
+# shellcheck shell=bash
+TMUX_POWERLINE_SEG_GCLOUD_ACTIVE_CONFIG_SYMBOL="${TMUX_POWERLINE_SEG_GCLOUD_ACTIVE_CONFIG_SYMBOL:-󱇶 }"
+
+generate_segmentrc() {
+  read -r -d '' rccontents <<EORC
+# Symbol for Google Cloud Active Configuration
+# export TMUX_POWERLINE_SEG_GCLOUD_ACTIVE_CONFIG_SYMBOL="${TMUX_POWERLINE_SEG_GCLOUD_ACTIVE_CONFIG_SYMBOL}"
+EORC
+  echo "$rccontents"
+}
+
 run_segment() {
-  if [ -r "$XDG_CONFIG_HOME/gcloud/active_config" ]; then
-    __config="$(cat "$XDG_CONFIG_HOME/gcloud/active_config")"
-    if [ "${#__config}" -gt 32 ]; then
-      __config="$(echo "$__config" | cut -c1-29)..."
-    fi
-    echo "󱇶 $__config"
-  fi
+  type gcloud >/dev/null 2>&1 || return 0
+  active_config="$(gcloud info --format='value(config.active_config_name)')"
+  [ -n "$active_config" ] && echo "${TMUX_POWERLINE_SEG_GCLOUD_ACTIVE_CONFIG_SYMBOL}${active_config}"
 }
